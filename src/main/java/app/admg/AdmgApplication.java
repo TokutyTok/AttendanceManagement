49bdfd8bb0294aab734374fb2c2d7a11885bf6d0
TakushiTokuyama <sokoninoruna@gmail.com>
2022-03-11 15:17:23 +0000

    first commit

---
+++ src/main/java/app/admg/AdmgApplication.java
@@ -0,0 +1,13 @@
+package app.admg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/admg/AdmgApplicationTests.java
@@ -0,0 +1,13 @@
+package app.admg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
